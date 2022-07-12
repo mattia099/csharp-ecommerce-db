b2e44a9fdf27a2ab42e76caf66f6f49aa7f28bdd
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-07-12 15:46:08 +0200

    Inseriti 3 prodotti

--- Program.cs
+++ Program.cs
@@ -2,6 +2,29 @@
 {
     static void Main() 
     {
+        using (EcommerceContext db = new EcommerceContext())
+        {
 
+            //// Create in maniera statica di prova
+            //Product prodotto1 = new Product { Name = "Scarpe Adidas", Description = "Scarpe comodissime", Price=45 };
+            //db.Add(prodotto1);
+            //Product prodotto2 = new Product { Name = "Scarpe Nike", Description = "Scarpe bellissime", Price = 60 };
+            //db.Add(prodotto2);
+            //Product prodotto3 = new Product { Name = "Cappello Puma", Description = "Scarpe comodissime", Price = 30 };
+            //db.Add(prodotto3);
+
+            ////salvataggio prodotti nel database
+            //db.SaveChanges(); 
+            
+        
+
+            // Read
+            Console.WriteLine("Recupero lista di Studenti");
+            List<Product> products = db.Product.OrderBy(student => student.Name).ToList();
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.Name);
+            }
+        }
     }
 }
